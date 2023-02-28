0d79a4618c65ce00c1c02794afd205eb04028136
Anwesha Chakravarty <74761763+AnweshaC21@users.noreply.github.com>
2023-02-28 11:43:07 +0530

    Update graph_traversal.c

--- graph_traversal.c
+++ graph_traversal.c
@@ -3,42 +3,41 @@
 
 int a[10][10], visited[10], n;
 
-void breadthSearchFrom(int);
-void depthSearchFrom(int);
+void breadthSearch(int);
+void depthSearch(int);
 void insertAdjacency();
 
 void main()
 {
     int i, choice;
         while (1)
-    {
+        {
         printf("\nGraph Traversals : ");
         printf("\n1. Breadth - First Traversal \n");
         printf("2. Depth - First Traversal \n");
         printf("3. Exit \n");
         printf("Enter the Choice : ");
-            scanf("%d", &choice);
+        scanf("%d", &choice);
         switch (choice)
         {
-            case 1:
-                insertAdjacency ();
-                printf("Nodes are visited in this order ");
-                for (i = 1; i <= n; i++)
-                    if (visited[i] == 0)
-                        breadthSearchFrom(i);
-                        printf("\n");
-                break;
-            case 2:
-                insertAdjacency ();
-                printf("Nodes are visited in this order ");
-                for (i = 1; i <= n; i++)
-                    if (visited[i] == 0)
-                        depthSearchFrom(i);
-                        printf("\n");
-                break;
-            default:
-                printf("End of run of program . . . ");
-                exit(1);
+        case 1:
+            insertAdjacency();
+            printf("Nodes are visited in this order ");
+            for (i = 1; i <= n; i++)
+                if (visited[i] == 0)
+                    breadthSearch(i);
+            printf("\n");
+            break;
+        case 2:
+            insertAdjacency();
+            printf("Nodes are visited in this order ");
+            for (i = 1; i <= n; i++)
+                if (visited[i] == 0)
+                    depthSearch(i);
+            printf("\n");
+            break;
+        default:
+            exit(1);
         }
     }
 }
@@ -46,21 +45,21 @@ void main()
 void insertAdjacency()
 {
     int i, j;
-    printf("\nEnter the number of Nodes : ");
-            scanf("%d",&n);
-        printf("Enter the adjacency matrix . . . \n");
-            for (i = 1; i <= n; i++)
-                for (j = 1; j <= n; j++)
-                    if (i!=j)
-                    {
-                        printf("Enter the value of %d, %d element : ", i, j);
-                            scanf("%d", &a[i][j]);
-                    }
+    printf("\nEnter the number of nodes: ");
+    scanf("%d",&n);
+    printf("Enter the adjacency matrix: \n");
+    for (i = 1; i <= n; i++)
+        for (j = 1; j <= n; j++)
+            if (i!=j)
+            {
+                printf("Enter the value of (%d, %d) element: ", i, j);
+                scanf("%d", &a[i][j]);
+            }
     for (i = 1; i <= n; i++)
         visited[i] = 0;
 }
 
-void breadthSearchFrom (int k)
+void breadthSearch(int k)
 {
     int i;
     printf("-> %d ", k);
@@ -71,14 +70,14 @@ void breadthSearchFrom (int k)
         {
             if (a[k][i] != 0)
             {
-                breadthSearchFrom(i);
+                breadthSearch(i);
             }
         }
         break;
     }
 }
 
-void depthSearchFrom (int k)
+void depthSearch(int k)
 {
     int i;
     printf("-> %d ", k);
@@ -86,5 +85,5 @@ void depthSearchFrom (int k)
     for (i = 1; i <= n; i++)
         if (visited[i] == 0)
             if (a[k][i] != 0)
-                depthSearchFrom(i);
+                depthSearch(i);
 }
